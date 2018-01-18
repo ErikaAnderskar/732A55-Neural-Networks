2f65b5e16d2e120f2118a7b9e6118007cbf7ae13
Herwan <herwineric@gmail.com>
2018-01-18 15:46:02 +0100

    hej

--- Lab1/kNN_lab.m
+++ Lab1/kNN_lab.m
@@ -1,23 +1,18 @@
 function [ labelsOut ] = kNN(X, k, Xt, Lt)
 
-matr = ones(1000, 1000);
-
-k = k;
-
 pred_labels = [];
-for i = 1:1000
+for i = 1:length(X)
     dist = sqrt(sum(Xt - X(:,i)) .^2); %eucledian
-    matr(i, :) = dist;
-    [M, I] = sort(matr(i, :));
-    ind = I(1:k); %taking out the lables of the minimum distance
+    %matr(i, :) = dist;
+    [M, I] = sort(dist);
+    inden = I(1:k); %taking out the lables of the minimum distance
     
     %because we have sorted the distances, the smallest distance wil be first
     %mode() will take the first value if labels 1 and 2 are the same amount
-    label = mode(Lt(ind)); %classified label
+    label = mode(Lt(inden)); %classified label
     pred_labels(i) = label;
 end
 
 labelsOut  = pred_labels;
 
 end
-
